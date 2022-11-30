c705c4dca7e29385b990331b50e916d98524f38e
Davg1700 <davg1700@gmail.com>
2022-11-29 22:19:04 -0700

    routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend)
+
+module.exports = router;
